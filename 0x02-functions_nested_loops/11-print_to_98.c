1be20f244c092327b73279fd1ae4503a07bebea9
Ezenwanne Oluchukwu <ezenwanneoluchukwu@yahoo.com>
2023-02-21 22:45:31 -0800

    11-print-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -23,6 +23,16 @@ void print_to_98(int n)
 		}
 		printf("\n");
 	}
+	else if (n < 0)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
 	else if (n > 98)
 	{
 		for (i = n ; i >= 98 ; i--)
