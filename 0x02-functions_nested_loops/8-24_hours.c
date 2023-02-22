e284c5013795386e1d86ebde7c1bce1751fcd4ff
Ezenwanne Oluchukwu <ezenwanneoluchukwu@yahoo.com>
2023-02-22 00:10:31 -0800

    8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,24 @@
 #include "main.h"
 /**
- * jack_bauer - prints every minute of the day
- *
- * Return: 0
- */
-
+  * jack_bauer - prints 24hrs time
+  *
+  * Return: 0
+  */
 void jack_bauer(void)
 {
-int a;
-int b;
+	int h;
+	int m;
 
-	for (a = 0; a <= 23; a++)
-	{	
-	for (b = 0; b <= 59; b++)
+	for (h = 0; h < 24; h++)
 	{
-	 _putchar (a / 10 + '0');
-	 _putchar (a % 10 + '0');
-	 _putchar (':');
-	 _putchar (b / 10 + '0');
-	 _putchar (b % 10 + '0');
-	 _putchar ('\n');
+		for (m = 0; m < 60; m++)
+		{
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
+		}
 	}
-
-	}
-
 }
-
