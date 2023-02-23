557cd2de20de101991e48d4d32faa46ecdf4ccea
Ezenwanne Oluchukwu <ezenwanneoluchukwu@yahoo.com>
2023-02-23 11:42:20 -0800

    2-largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,24 +12,8 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
-	{
-	largest = a;
-	}
-	else if (a > c && c > b)
-		largest = a;
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else if (b > c && c > a)
-		largest = b;
-	else if (c > a && a > b)
-		largest = c;
-	else
-	{
-	largest = c;
-	}
-
+	largest = (a > b) ? a : b;
+	largest = (largest > c) ? largest : c;
 	return (largest);
 }
+
