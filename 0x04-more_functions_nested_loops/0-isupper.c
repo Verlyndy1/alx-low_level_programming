ea07a94d3f8c5ecbc1d5313c6fa6423e17f08267
Ezenwanne Oluchukwu <ezenwanneoluchukwu@yahoo.com>
2023-02-23 11:29:43 -0800

    0-isuper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
  * _isupper - define if a character is uppercase
  *
- * Description: prints the alphabet with _putchar
- * @c: character
- * Return: 0
+ * Description: Prints the alphabet with _putchar
+ * @c: charater
+ * Return: void
  */
-int _isupper(int c)
 
+int _isupper(int c)
 {
-	if (c <= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
