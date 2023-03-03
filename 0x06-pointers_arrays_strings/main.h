cb1a9254d40560fa0ff60d1af78c4376409e3f0a
verlyndy1 <ezenwanneoluchukwu@yahoo.com>
2023-03-03 02:56:58 -0800

    0-strcat

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+char *_strncat(char *dest, char *src, int n);
+char *cap_string(char *);
+char *string_toupper(char *);
+void reverse_array(int *a, int n);
+int _strcmp(char *s1, char *s2);
+char *_strncpy(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
+#endif
