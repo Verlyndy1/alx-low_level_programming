6b38b9a921ef9bf54727ae2d63c3e70fa265a3de
verlyndy1 <ezenwanneoluchukwu@yahoo.com>
2023-04-09 23:44:16 -0700

    0-binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,11 +1,19 @@
+/*
+ * File_name: 0-binary_to_uint.c
+ * Created: 07-04-2023
+ * Auth: Ezenwanne Marylynda Oluchukwu
+ * Size: undefined
+ * Project: alx-low_level_programming
+ */
+
 #include "main.h"
 
 /**
- * binary_to_unit - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 or 1 - 0.
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 chars.
  *
- * Return: If b is NULL or contains char not 0 or 1 - 0.
- * Otherwise -  the converted number.
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
+ *         Otherwise - the converted number.
  */
 unsigned int binary_to_uint(const char *b)
 {
